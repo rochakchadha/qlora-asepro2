491a3a413308883292210346b1aeeebaa35d20f7
rochakchadha <83778749+rochakchadha@users.noreply.github.com>
2023-12-08 01:27:39 -0500

    testing with grad_checkpt_disabled

--- qlora.py
+++ qlora.py
@@ -20,8 +20,10 @@ model = AutoModelForCausalLM.from_pretrained(
     device_map='auto',
     quantization_config=bnb_config
 )
+gradient_checkpointing = False
 model.config.use_cache = False
-model = prepare_model_for_kbit_training(model, use_gradient_checkpointing=True)  # https://github.com/huggingface/peft/blob/52ff0cde9f2cc64059e171c2cfd94512914c85df/src/peft/utils/other.py#L92
+model = prepare_model_for_kbit_training(model, use_gradient_checkpointing=gradient_checkpointing)  # https://github.com/huggingface/peft/blob/52ff0cde9f2cc64059e171c2cfd94512914c85df/src/peft/utils/other.py#L92
+
 tokenizer = AutoTokenizer.from_pretrained(model_path)
 tokenizer.pad_token = tokenizer.eos_token
 
@@ -55,7 +57,7 @@ eval_dataset = shuffled_dataset.select(range(300))
 
 training_arguments = TrainingArguments(
     output_dir=output_dir,
-    per_device_train_batch_size=6,
+    per_device_train_batch_size=3,
     gradient_accumulation_steps=4,
     learning_rate=2e-4,
     max_grad_norm=1.0,
