fa4aac07653cfeb718bc682f7c7ad130994529bf
rochakchadha <83778749+rochakchadha@users.noreply.github.com>
2023-12-14 00:36:34 -0500

    added requirements.txt and setup.sh

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Install Python packages from requirements.txt
+echo "Installing Python packages from requirements.txt..."
+export FLASH_ATTENTION_SKIP_CUDA_BUILD=TRUE
+pip3 install -r requirements.txt
+
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | bash
+apt-get install git-lfs
+
+echo "Cloning the Mistral-7B-v0.1 repository..."
+cd ./qlora-asepro2/base_model
+git clone https://huggingface.co/mistralai/Mistral-7B-v0.1
+
+wandb login
