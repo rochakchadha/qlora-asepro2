024c36e97efb38c76fb36f36e470fa7e93cfaac9
rochakchadha <83778749+rochakchadha@users.noreply.github.com>
2023-12-13 23:50:30 -0500

    update to qlora.py to reflect weightsandbiases. min hyper_param changes

--- prep_dataset.py
+++ prep_dataset.py
@@ -1,10 +1,17 @@
 from datasets import load_dataset, Dataset
 from random import randint
 import torch
+from collections import defaultdict
 from transformers import AutoModelForCausalLM, AutoTokenizer, DataCollatorForLanguageModeling
 
 # load tokenizer from disk
-tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-v0.1")
+tokenizer = AutoTokenizer.from_pretrained(
+    "mistralai/Mistral-7B-v0.1",
+    padding_side="left",
+    add_eos_token=True,
+    add_bos_token=True,
+    )
+tokenizer.pad_token = tokenizer.eos_token
 
 #load original dataset from hub and save to disk
 data = load_dataset("./datasets/databricks-dolly-15k", split="train")
@@ -18,23 +25,47 @@ def format_dolly(sample):
     prompt = "\n\n".join([i for i in [instruction, context, response] if i is not None])
     return prompt
 
-# template dataset to add prompt to each sample
+# check the distribution of the dataset. First we tokenize and then we check the length of the tokenized prompt
+
+def token_length(example):
+    result = tokenizer(format_dolly(example),return_tensors="pt", return_attention_mask=False)
+    length_of_sequence = result.input_ids.shape[1]
+    return length_of_sequence
+
+# Define the buckets
+buckets = {
+    "<100": 0,
+    "100-256": 0,
+    "256-512": 0,
+    "512-1024": 0,
+    ">1024": 0
+}
+
+# Function to determine the bucket for a given length
+def determine_bucket(length):
+    if length < 100:
+        return "<100"
+    elif length <= 256:
+        return "100-256"
+    elif length <= 512:
+        return "256-512"
+    elif length <= 1024:
+        return "512-1024"
+    else:
+        return ">1024"
+
+    # Iterate over the dataset and count
+    counter = 0
+    for example in data:
+            length = token_length(example)
+            bucket = determine_bucket(length)
+            buckets[bucket] += 1
+            counter += 1
+
+    # Display the distribution
+    print("Distribution of sequence lengths:")
+    for bucket, count in buckets.items():
+        print(f"{bucket}: {count}")
 
 
-def template_dataset(sample):
-    sample["text"] = f"{tokenizer.bos_token}{format_dolly(sample)}{tokenizer.eos_token}"
-    return sample
-
-# apply prompt template per sample
-dataset = data.map(template_dataset, remove_columns=list(data.features))
-# print random sample
-print(dataset[randint(0, len(dataset))]["text"])
-
-max_length = 1024
-
-# tokenize dataset
-tok_dataset = dataset.map(lambda sample: tokenizer(sample["text"], max_length=max_length, truncation=True), batched=True, remove_columns='text')
-
-#save tokenized dataset to disk
-tok_dataset.save_to_disk("./train_dataset/")
 
--- qlora.py
+++ qlora.py
@@ -2,10 +2,13 @@ import torch
 from transformers import (AutoModelForCausalLM, AutoTokenizer, Trainer, TrainingArguments, DataCollatorForLanguageModeling, BitsAndBytesConfig)
 from datasets import (load_dataset, Dataset)
 from peft import (LoraConfig, get_peft_model, prepare_model_for_kbit_training, PeftModel)
-
+import wandb 
+import os
+from datetime import datetime
 
 model_path = "./base_model/Mistral-7B-v0.1/"
 training_path = "./train_dataset/"
+eval_path = "./eval_dataset/"
 output_dir = "./lora_checkpoints"
 
 bnb_config = BitsAndBytesConfig(
@@ -18,7 +21,7 @@ bnb_config = BitsAndBytesConfig(
 model = AutoModelForCausalLM.from_pretrained(
     model_path,
     device_map='auto',
-    use_flash_attention_2=True,
+    attn_implementation="flash_attention_2",
     quantization_config=bnb_config
 )
 gradient_checkpointing = True
@@ -28,14 +31,22 @@ model = prepare_model_for_kbit_training(model, use_gradient_checkpointing=gradie
 tokenizer = AutoTokenizer.from_pretrained(model_path)
 tokenizer.pad_token = tokenizer.eos_token
 
+# set the wandb project where this run will be logged
+os.environ["WANDB_PROJECT"]="Mistral Qlora"
+
+# save your trained model checkpoint to wandb
+os.environ["WANDB_LOG_MODEL"]="true"
+
+# turn off watch to log faster
+os.environ["WANDB_WATCH"]="false"
 # setup LoRA
 
 #as per https://wandb.ai/vincenttu/finetuning_mistral7b/reports/Fine-tuning-Mistral-7B-with-W-B--Vmlldzo1NTc3MjMy
-lora_modules = ['o_proj', 'up_proj', 'down_proj', 'q_proj', 'v_proj', 'gate_proj', 'k_proj']
+lora_modules = ['o_proj', 'up_proj', 'down_proj', 'q_proj', 'v_proj', 'gate_proj', 'k_proj', 'lm_head']
 
-lora_alpha = 16
-lora_dropout = 0.1
-lora_r = 64
+lora_alpha = 64
+lora_dropout = 0.05
+lora_r = 16
 
 peft_config = LoraConfig(
     lora_alpha=lora_alpha,
@@ -50,24 +61,34 @@ model.print_trainable_parameters()
 
 #load dataset from disk and define eval dataset
 dataset = Dataset.load_from_disk(training_path)
+eval_dataset = Dataset.load_from_disk(eval_path)
+
 #shuffled_dataset = dataset.shuffle(seed=42)
 #eval_dataset = shuffled_dataset.select(range(300))
 
+project = "Dolly-Qlora"
+base_model_name = "mistral"
+run_name = base_model_name + "-" + project
 
 # define training argument
 
 training_arguments = TrainingArguments(
     output_dir=output_dir,
-    per_device_train_batch_size=6,
-    gradient_accumulation_steps=2,
-    learning_rate=2e-4,
+    warmup_steps=1,
+    per_device_train_batch_size=12,
+    gradient_accumulation_steps=1,
+    learning_rate=2e-5,
     lr_scheduler_type="constant",
     warmup_ratio=0.03,
     num_train_epochs=1,
     fp16=True,
     save_steps=75,
     logging_steps=20,
-    save_strategy="steps",
+    report_to="wandb",
+    evaluation_strategy="steps",
+    do_eval=True,
+    run_name=f"{run_name}-{datetime.now().strftime('%Y-%m-%d-%H-%M')}",
+    save_strategy="steps"
 )
 
 trainer = Trainer(
@@ -75,6 +96,6 @@ trainer = Trainer(
     args=training_arguments,
     data_collator=DataCollatorForLanguageModeling(tokenizer, mlm=False),
     train_dataset=dataset,
-    #eval_dataset=eval_dataset,
+    eval_dataset=eval_dataset,
 )
 trainer.train()
